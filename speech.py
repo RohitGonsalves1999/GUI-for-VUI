1504edd1adc9ef63903f94c6a1a5f292f771e675
RohitGonsalves1999 <32949288+RohitGonsalves1999@users.noreply.github.com>
2019-10-22 01:00:29 +0530

    Added All Files
    Add A new label to the GUI and integrate with wit.ai . For now use the Google voice engine ill change that later

---
+++ speech.py
@@ -0,0 +1,68 @@
+import tkinter as tk
+import requests
+import sounddevice as sd
+from scipy.io.wavfile import write
+import numpy as np
+import soundfile as sf
+import wavio
+import speech_recognition as sr
+
+HEIGHT = 500
+WIDTH = 600
+
+def speech_recognizer():
+    record_audio(5)
+    label['text'] = recognise_speech()
+
+def recognise_speech(filename = 'output.wav'):
+    r = sr.Recognizer()
+    data = sr.AudioFile('output.wav')
+    with data as source:
+        audio = r.record(source)
+    return (r.recognize_google(audio))
+
+
+def record_audio(duration,sample_rate = 44100):
+    fs = sample_rate  # Sample rate
+    seconds = duration  # Duration of recording
+    
+    print('Baat kar na L****')
+    myrecording = sd.rec(int(seconds * fs),fs,1)
+    sd.wait()  # Wait until recording is finished
+    print('ruk na bsdk')
+    #print(myrecording.flatten())
+    #print(myrecording)
+
+    '''write('output.wav', fs, myrecording)  # Save as WAV file 
+    data, fs = sf.read('output.wav')
+    print(data)
+    sf.write('output.flac',data,fs)'''
+
+    wavio.write('output.wav',myrecording,fs,sampwidth=2)
+
+
+root = tk.Tk()
+
+canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH)
+canvas.pack()
+
+background_image = tk.PhotoImage(file='siri.png')
+background_label = tk.Label(root, image=background_image)
+background_label.place(relwidth=1, relheight=1)
+
+frame = tk.Frame(root, bg='#80c1ff')
+frame.place(relx=0.5, rely=0.1, relwidth=0.5, relheight=0.1, anchor='n')
+
+#entry = tk.Entry(frame, font=40)
+#entry.place(relwidth=0.65, relheight=1)
+
+button = tk.Button(frame, text="Speak", font=10, command=lambda: speech_recognizer())
+button.place(relx=0.5, relheight=1, relwidth=1,anchor = 'n')
+
+lower_frame = tk.Frame(root, bg='#80c1ff')
+lower_frame.place(relx=0.5, rely=0.25, relwidth=0.75, relheight=0.6, anchor='n')
+
+label = tk.Label(lower_frame)
+label.place(relwidth=1, relheight=1)
+
+root.mainloop()
